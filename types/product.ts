2f932c0d59404d4b672951eb55a84531213dbcdb
Islam Abouelregal <eslamalbadawy2000@gmail.com>
2026-06-07 02:28:30 +0300

    Add product type and Firestore product helper

---
+++ types/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: string;
+  name: string;
+  category: string;
+  price: number;
+  stock: number;
+  image: string;
+  description: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
